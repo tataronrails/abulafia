a8115663fb9564a06a8227b6ff34d40b341a89fc
almazom <almazomru@gmail.com>
2012-12-15 00:40:37 +0400

    config.action_mailer.default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ EOffice::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'almazom.com:7004' }
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
