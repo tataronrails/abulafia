4ad0e70e9ab545103ba903643b86079d0d1b2a79
almazom <almazomru@gmail.com>
2012-12-27 10:28:16 +0400

    [fix] seng jabber only on not development env

--- app/models/jabber_bot.rb
+++ app/models/jabber_bot.rb
@@ -4,27 +4,30 @@ class JabberBot
 
   def initialize(*args)
     @options = args.extract_options!
-    self.user  = @options.delete(:user ).presence || nil
-    self.message  = @options.delete(:message ).presence || nil
+    self.user = @options.delete(:user).presence || nil
+    self.message = @options.delete(:message).presence || nil
     self
   end
 
   def send_message
-    if self.user.hc_user_id.nil?
-      login = KEYS['bot']['gmail'][0]
-      pass = KEYS['bot']['gmail'][1]
-      adress = self.user.email
-    else
-      login = KEYS['bot']['hipchat'][0]
-      pass = KEYS['bot']['hipchat'][1]
-      adress ="#{KEYS['bot']['hipchat'][2]}_#{self.user.hc_user_id}@#{KEYS['bot']['hipchat'][3]}"
+    unless Rails.env.eql? "development"
+
+      if self.user.hc_user_id.nil?
+        login = KEYS['bot']['gmail'][0]
+        pass = KEYS['bot']['gmail'][1]
+        adress = self.user.email
+      else
+        login = KEYS['bot']['hipchat'][0]
+        pass = KEYS['bot']['hipchat'][1]
+        adress ="#{KEYS['bot']['hipchat'][2]}_#{self.user.hc_user_id}@#{KEYS['bot']['hipchat'][3]}"
+      end
+      robot = Jabber::Client::new(Jabber::JID::new(login))
+      robot.connect
+      robot.auth(pass)
+      message = Jabber::Message::new(adress, self.message)
+      message.set_type(:chat)
+      robot.send message
     end
-    robot = Jabber::Client::new(Jabber::JID::new(login))
-    robot.connect
-    robot.auth(pass)
-    message = Jabber::Message::new(adress, self.message)
-    message.set_type(:chat)
-    robot.send message
   end
 
   #jb = JabberBot.new( :user => user12, :message => "drgdrgdfgdf")
