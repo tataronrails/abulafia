2a3763ecd38c1ecc6ec4d3dbcacee476ce2d1073
Che Onza <ivcheg@gmail.com>
2013-01-08 11:57:48 +0400

    hipchat api testing

--- app/models/jabber_bot.rb
+++ app/models/jabber_bot.rb
@@ -1,8 +1,12 @@
 class JabberBot
+  require 'hipchat-api'
   # To change this template use File | Settings | File Templates.
   attr_accessor :user, :message
 
   include Rails.application.routes.url_helpers
+
+
+
 
   def initialize(*args)
     @options = args.extract_options!
@@ -12,35 +16,45 @@ class JabberBot
   end
 
   def send_message
-    if self.user.present?
-      login = KEYS['bot']['gmail'][0]
-      pass = KEYS['bot']['gmail'][1]
-      gtalk_bot = Jabber::Client::new(Jabber::JID::new(login))
-      gtalk_bot.connect
-      gtalk_bot.auth(pass)
+    hipchat_api = HipChat::API.new('94ecc0337c81806c0d784ab0352ee7')
+    #if self.user.present?
+    #  login = KEYS['bot']['gmail'][0]
+    #  pass = KEYS['bot']['gmail'][1]
+    #  gtalk_bot = Jabber::Client::new(Jabber::JID::new(login))
+    #  gtalk_bot.connect
+    #  gtalk_bot.auth(pass)
+    #
+    #  login = KEYS['bot']['hipchat'][0]
+    #  pass = KEYS['bot']['hipchat'][1]
+    #  hipchat_bot = Jabber::Client::new(Jabber::JID::new(login))
+    #  hipchat_bot.connect
+    #  hipchat_bot.auth(pass)
+    #
+    #  self.user.each  do |u|
+    #    if u.hc_user_id.nil?
+    #      robot = gtalk_bot
+    #      adress = u.email
+    #    else
+    #      robot = hipchat_bot
+    #      adress ="#{KEYS['bot']['hipchat'][2]}_#{u.hc_user_id}@#{KEYS['bot']['hipchat'][3]}"
+    #    end
+    #    message = Jabber::Message::new(adress, self.message)
+    #    message.set_type(:chat)
+    #    robot.send message
+    #  end
+    #
+    #  gtalk_bot.close
+    #  hipchat_bot.close
+    #end
+    rooms = hipchat_api.rooms_list
+    #raise rooms.to_json
+    #room_id = "#{KEYS['bot']['hipchat'][2]}_#{self.user.hc_user_id}"
+    #raise (room = hipchat_api.rooms_create('abulafia' , self.user.hc_user_id, privacy = 'private', topic = '', guest_access = 0)).to_json
 
-      login = KEYS['bot']['hipchat'][0]
-      pass = KEYS['bot']['hipchat'][1]
-      hipchat_bot = Jabber::Client::new(Jabber::JID::new(login))
-      hipchat_bot.connect
-      hipchat_bot.auth(pass)
-
-      self.user.each  do |u|
-        if u.hc_user_id.nil?
-          robot = gtalk_bot
-          adress = u.email
-        else
-          robot = hipchat_bot
-          adress ="#{KEYS['bot']['hipchat'][2]}_#{u.hc_user_id}@#{KEYS['bot']['hipchat'][3]}"
-        end
-        message = Jabber::Message::new(adress, self.message)
-        message.set_type(:chat)
-        robot.send message
-      end
-
-      gtalk_bot.close
-      hipchat_bot.close
-    end
+    room_id = rooms[:rooms].find(:name => 'abulafia').first.room_id
+    message = self.message
+    from = 'abulafia'
+    raise (hipchat_api.rooms_message(room_id, from, message, notify = 0, color = 'yellow', message_format = 'html')).to_json
   end
 
 
