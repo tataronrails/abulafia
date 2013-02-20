04acd21feec9f45b99493f7b97cce634da3aa266
almazom <almazomru@gmail.com>
2013-02-20 16:38:15 +0400

    remove resources minus and plus transaction

--- app/controllers/plus_transactions_controller.rb
+++
@@ -1,5 +0,0 @@
-class PlusTransactionsController < InheritedResources::Base
-  load_and_authorize_resource
-
-
-end
--- app/models/plus_transaction.rb
+++
@@ -1,6 +0,0 @@
-class PlusTransaction < ActiveRecord::Base
-  attr_accessible :desc, :source_user, :value
-  default_scope :order => 'created_at DESC'
-
-  has_many :minus_transactions
-end
