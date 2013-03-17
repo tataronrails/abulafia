3e90e47e001bdec4d49b1b00c8aff9e110d0b4de
Pavel <pavel.evst@gmail.com>
2013-03-17 15:23:05 +0800

    Refactor Tasks#my page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,8 +6,14 @@ class TasksController < ApplicationController
 
   def my
     tasks = current_user.my_tasks
-    @projects = Project.where(:id => [tasks.map(&:project_id).uniq]).order("name DESC")
-
+    @projects = Project.where(:id => [tasks.map(&:project_id).uniq]).includes(:tasks).order("name DESC")
+    @tasks = Task
+      .where(:assigned_to => current_user, :project_id => @projects.map(&:id))
+      .includes(:owner, :discussion)
+      .order(:created_at).delete_if do |t|
+        (t.status == 2) || (t.finished_at.present?) || (t.hours_worked_on_task.present?)
+      end
+    @comments_count = Task.where(:id => @tasks.map(&:id)).joins(:comments).group("tasks.id").count("comments.id")
   end
 
   def sms_ping
