3ef06be3eed06971b83c27f45f48e6722c0162a1
Andrei Dziahel <develop7@develop7.info>
2013-04-13 17:02:28 +0300

    fix broken root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 EOffice::Application.routes.draw do
 
-  ActiveAdmin.routes(self)
-  devise_for :admin_users, ActiveAdmin::Devise.config
-
   resources :strikes
 
 
@@ -131,6 +128,10 @@ EOffice::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'projects#index'
+
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
 
   # See how all your routes lay out with "rake routes"
 
